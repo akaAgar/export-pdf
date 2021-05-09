f398d5ab082ef206e57d1e947d2f65207645f797
Agar <47314805+akaAgar@users.noreply.github.com>
2021-05-09 19:30:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Export PDF
+
+Command-line tool designed to export a PDF from a .zip file and save it as a docx
+Designed for a very specific use, but if it may be of use to someone...
+
+Released under GPL v3
---
+++ src/ExportPDFProgram.cs
@@ -0,0 +1,55 @@
+﻿using ICSharpCode.SharpZipLib.Zip;
+using SautinSoft;
+using System;
+using System.IO;
+
+namespace ExportPDF
+{
+    public sealed class ExportPDFProgram
+    {
+        private static void Main(params string[] args)
+        {
+            if (args.Length == 0)
+                Console.WriteLine($"Aucun fichier fourni.");
+
+            foreach (string filePath in args)
+            {
+                if (!File.Exists(filePath))
+                {
+                    Console.WriteLine($"File \"{Path.GetFileName(filePath)}\" n'existe pas.");
+                    continue;
+                }
+
+                using (ZipFile zipFile = new ZipFile(filePath))
+                {
+                    bool pdfFound = false;
+
+                    foreach (ZipEntry zipEntry in zipFile)
+                    {
+                        if (!zipEntry.IsFile) continue;
+                        if (Path.GetFileName(zipEntry.Name).ToLowerInvariant().EndsWith(".pdf"))
+                        {
+                            PdfFocus pdfFocus = new PdfFocus();
+                            pdfFocus.OpenPdf(zipFile.GetInputStream(zipEntry));
+                            pdfFocus.WordOptions.Format = PdfFocus.CWordOptions.eWordDocument.Docx;
+                            int result = pdfFocus.ToWord($"{Path.GetFileNameWithoutExtension(filePath)}.docx");
+                            Console.WriteLine($"Exporté vers \"{Path.GetFileNameWithoutExtension(filePath)}.docx\".");
+
+                            pdfFound = true;
+                            break;
+                        }
+                    }
+
+                    if (!pdfFound)
+                        Console.WriteLine($"File \"{Path.GetFileName(filePath)}\" ne contient pas de PDF.");
+                }
+            }
+
+#if DEBUG
+            Console.WriteLine();
+            Console.WriteLine("Pressez une touche pour fermer cette fenêtre...");
+            Console.ReadKey();
+#endif
+        }
+    }
+}
